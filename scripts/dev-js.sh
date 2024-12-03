0b73500a15a0304272dd1f4445a0acc11c269cfe
Gustavo Gondim <gustavospgondim@gmail.com>
2024-12-03 16:58:23 +0000

    fix(dev-js): volta was not sourced

--- scripts/dev-js.sh
+++ scripts/dev-js.sh
@@ -4,9 +4,12 @@
 echo "\n\nVOLTA ======================================\n"
 curl https://get.volta.sh | bash
 
+source ~/.profile
+
 echo "\n\nBUN & PNPM ======================================\n"
 volta install bun
 volta install pnpm
 
 echo "VOLTA_FEATURE_PNPM=1" >> ~/.profile
 
+source ~/.profile
